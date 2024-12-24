93cf8e6a322f6d5dd0d84b0349c70f8e6f06abcc
vipinputhanveetil <cloudtechbyteswithvipin@gmail.com>
2024-12-24 17:28:18 +1100

    Update sql_unpivot.sql

--- sql_unpivot.sql
+++ sql_unpivot.sql
@@ -1,3 +1,4 @@
+#Exercise:1
 CREATE OR REPLACE TABLE monthly_sales(
   empid INT,
   dept TEXT,
@@ -24,3 +25,31 @@ SELECT *
   FROM monthly_sales
     UNPIVOT INCLUDE NULLS (sales FOR month IN (jan, feb, mar, apr))
   ORDER BY empid;
+
+
+#Exercise:2
+CREATE TABLE sales (
+    Product VARCHAR(50),
+    Q1 INT,
+    Q2 INT,
+    Q3 INT,
+    Q4 INT
+);
+
+INSERT INTO sales (Product, Q1, Q2, Q3, Q4)
+VALUES 
+('ProductA', 100, 150, 200, 250),
+('ProductB', 300, 350, 400, 450);
+
+SELECT * FROM sales;
+
+SELECT 
+    product,
+    quarter,
+    salesAmount
+FROM
+    (SELECT Product, Q1, Q2, Q3, Q4 FROM sales) 
+    AS sourcetable
+UNPIVOT
+    (salesamount FOR quarter IN (Q1, Q2, Q3, Q4)) 
+    AS unpivotedtable;
